9e0505a3ba99af24808e7493e19ee3c6eabe68a0
Ayushi Shah <as17726@nyu.edu>
2023-09-29 17:08:00 -0400

    Update sketch.js

--- 0/sketch.js
+++ 0/sketch.js
@@ -4,26 +4,25 @@ function setup() {
   strokeWeight(0);
 }
 
-function secondincrease() {
-  fill(random(0,255),255,255);
-  ellipse(x, y,10,10);
-  y = y+10; 
-}
+//function secondincrease(x1,y1) {
+  //fill(random(0,255),255,255);
+  //ellipse(x1, y1,10,10);
+//}
 
 function draw() {
   let x = 50;
   let y = 50; 
+  
   let secondsNow = second();
-  
- 
-  
-  }
+  fill(random(0,255),255,255);
+  ellipse(x,y,10,10);
+}
+
   
 
   
   
-  //let myDiameter = map(secondsNow, 0, 59, 100, height);
-  //ellipse(width / 2, height / 2, myDiameter, myDiameter);
+  
 
 
   
