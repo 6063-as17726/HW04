451f9428d75edf509c1f525b2a4ee2858a2a307c
Ayushi Shah <as17726@nyu.edu>
2023-09-29 20:37:34 -0400

    Attempt 02 - sketch 0

--- 0/sketch.js
+++ 0/sketch.js
@@ -2,15 +2,28 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
   strokeWeight(0);
+  //let x = 50;
+  //let y = 50;  
 }
+ 
+//let lastSecondDrawn = second(); 
 
 function draw() {
-  let x = 50;
-  let y = 50; 
-  let secondsNow = second()
-  fill(random(0,255),255,255);
-  ellipse(x, y,10,10);
-  y+=10;
+fill(125,125,125); 
+ellipse(width/2,height/2,10,10); 
+
+
+
+
+
+
+  //let secondsNow = second()
+  //if(lastSecondDrawn!=secondsNow){
+    //fill(random(0,255),255,255);
+    //ellipse(x, y,10,10);
+   // y = y + 10;
+    //lastSecondDrawn = secondsNow; 
+  //}
 }
 
 
