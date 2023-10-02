47028bf11c4480cbadf7f7ad1dbf2e325a91a5c5
Ayushi Shah <as17726@nyu.edu>
2023-10-01 20:15:07 -0400

    Attempt 04 - fixing spacing in sketch 0

--- 0/sketch.js
+++ 0/sketch.js
@@ -4,8 +4,8 @@ function setup() {
   strokeWeight(0);
 }
  
-let x = 25;
-let y = 25;  
+let x = 0;
+let y = 0;  
 
 let lastSecondDrawn = 0;
 let lastMinuteDrawn = 0;  
@@ -18,17 +18,17 @@ function draw() {
   if(lastHourDrawn!=hoursNow){
     background(255);
     lastHourDrawn = hoursNow;
-    x = 25; 
+    x = 0; 
   }
   if(lastMinuteDrawn!=minutesNow){
     x = x + windowWidth/60; 
-    y = 25; 
+    y = 0; 
     lastMinuteDrawn = minutesNow; 
   }
   if(lastSecondDrawn!=secondsNow){
     fill(random(50,255),random(200,255),random(200,255));
     ellipse(x, y,10,10);
-    y = y + 10;
+    y = y + windowHeight/60;
     lastSecondDrawn = secondsNow;
   }
 }
